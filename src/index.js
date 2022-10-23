09abd7e6a65797de3099b48ee47c5bb8a65e406e
Aleksandr Mihasenko <aleksandr.mihasenko@gmail.com>
2022-10-23 14:09:33 +0400

    refactoring

--- src/index.js
+++ src/index.js
@@ -1,25 +1,9 @@
 const TelegramApi = require('node-telegram-bot-api');
+const { gameOptions, againOptions } = require('./options');
 
-const token = '5673654601:AAF4x8JsPaJXUj6Ab7iDbZhxF6JCPop6r64';
+const token = '';
 const bot = new TelegramApi(token, { polling: true });
 const chats = {};
-const gameOptions = {
-  reply_markup: JSON.stringify({
-    inline_keyboard: [
-      [{ text: '1', callback_data: '1' }, { text: '2', callback_data: '2' }, { text: '3', callback_data: '3' }],
-      [{ text: '4', callback_data: '4' }, { text: '5', callback_data: '5' }, { text: '6', callback_data: '6' }],
-      [{ text: '7', callback_data: '7' }, { text: '8', callback_data: '8' }, { text: '9', callback_data: '9' }]
-    ]
-  })
-}
-
-const againOptions = {
-  reply_markup: JSON.stringify({
-    inline_keyboard: [
-      [{ text: 'Play again', callback_data: '/again' }],
-    ]
-  })
-}
 
 const startGame = async (chatId) => {
   await bot.sendMessage(chatId, `Try to gas a number from 0 to 9.`);
